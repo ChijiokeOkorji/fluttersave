f3f0a626025f4276c8bd986a6ad3be5ab85994ec
adekunle odujoko <kodujoko@gmail.com>
2022-09-29 20:51:34 +0100

    backend update

--- utils/transactions.js
+++ utils/transactions.js
@@ -179,7 +179,8 @@ const bankWithdrawal = async ({
           narration: summary,
           currency: "NGN",
           reference: reference,
-          callback_url: "https://fluttersave.herokuapp.com/home",
+          callback_url:
+            "https://webhook.site/91c8e065-1223-488a-8a19-464af49cb414",
           debit_currency: "NGN",
           meta: {
             email: fromEmail,
